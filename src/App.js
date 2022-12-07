0802c430d429f7d767a7105762ca5aa749154a3b
phptuts <glaserpower@gmail.com>
2022-12-06 17:14:41 -0800

    preview ex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>(Noah Changed)Secret {process.env.REACT_APP_SECRET_CODE}</h1>;
+  return <h1>(Noah PR)Secret {process.env.REACT_APP_SECRET_CODE}</h1>;
 }
 
 export default App;
